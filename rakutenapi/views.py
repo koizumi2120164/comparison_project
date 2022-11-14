44d7d0e7492ca6693ced4a103a19a97e6e909cc9
amelliale <thuy.jp2019@gmail.com>
2022-11-14 09:27:43 +0900

    rakutenapi

--- rakutenapi/views.py
+++ rakutenapi/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import View
 from .forms import SearchForm
+import json
 import requests
+
+SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404?format=json&applicationId=[1059665974949401126]'
+
+def get_api_data(params):
+    api = requests.get(SEARCH_URL, params=params)
+    result = json.loads(api)
+    items = result['Items']
+    return items
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
@@ -9,4 +18,35 @@ class IndexView(View):
 
         return render(request, 'app/index.html', {
             'form':form,
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = SearchForm(request.POST or None)
+
+        if form.is_valid():
+            keyword = form.clean_data['title']
+            params = {
+                'title': keyword,
+                'hits': 28
+            }
+            items = get_api_data(params)
+            book_data = []
+            for i in items:
+                item = i['Item']
+                title = item['title']
+                image = item['largeImageUrl']
+                isbn = item['isbn']
+                query = {
+                    'title': title,
+                    'image': image,
+                    'isbn': isbn,
+                }
+                book_data.append(query)
+
+            return render(request, 'book.html', {
+                'book_data': book_data,
+                'keyword': keyword
+            })
+        return render (request, 'testindex.html', {
+            'form': form
         })
