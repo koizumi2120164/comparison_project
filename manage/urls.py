044fcb4220a38dc977bd6853d8e21b2343599561
Nogosi1228 <ikb2120222@stu.o-hara.ac.jp>
2022-11-29 11:58:18 +0900

    master

--- manage/urls.py
+++ manage/urls.py
@@ -6,7 +6,7 @@ app_name = 'manage'
 urlpatterns = [
    
     path('', views.IndexView.as_view(), name="index"),
-    path('manage_top/', views.Managetopview.View.as_view(), name="manage_top"),
-    path('manage_toptable/', views.Managetoptableview.View.as_view(), name="manage_toptable"),
-    path('manage_searchresult/', views.Managesearchresultview.View.as_view(), name="manage_searchresult"), 
+    path('manage_top/', views.Managetopview.as_view(), name="manage_top"),
+    path('manage_toptable/', views.Managetoptableview.as_view(), name="manage_toptable"),
+    path('manage_searchresult/', views.Managesearchresultview.as_view(), name="manage_searchresult"), 
 ]
