75049678b9cb4eb73e5a9072af1c6fbb03c1eba7
Nogosi1228 <ikb2120222@stu.o-hara.ac.jp>
2022-11-30 12:26:52 +0900

    master

--- manage/urls.py
+++ manage/urls.py
@@ -4,9 +4,7 @@ from .import views
 
 app_name = 'manage'
 urlpatterns = [
-   
-    path('', views.IndexView.as_view(), name="index"),
-    path('manage_top/', views.Managetopview.as_view(), name="manage_top"),
-    path('manage_toptable/', views.Managetoptableview.as_view(), name="manage_toptable"),
-    path('manage_searchresult/', views.Managesearchresultview.as_view(), name="manage_searchresult"), 
+    path('top/', views.age, name="top"),
+    path('table/', views.ManageTableView.as_view(), name="table"),
+    path('search/', views.SearchView.as_view(), name="search"),
 ]
