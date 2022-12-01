a08e4f8eafc14b0546cb16c2f90e4bdbb9f1c187
Nogosi1228 <ikb2120222@stu.o-hara.ac.jp>
2022-12-01 15:19:22 +0900

    master

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
-def account(request):
-
-    if  request.user.is_staff:
-        return redirect('top/' )
+def index(request):
+    if not request.user.is_superuser:
+     return render(request,'manage/manage_top.html')  
    
 
