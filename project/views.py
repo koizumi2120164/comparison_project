07e4f6b6144d0b4e8a7de148028d872e72b64540
koizumi2120164 <ikb2120164@stu.o-hara.ac.jp>
2023-02-14 10:56:48 +0900

    slug

--- project/views.py
+++ project/views.py
@@ -135,7 +135,7 @@ def Ajax_ch_product(request, slug):
         wish.delete()
         context['method'] = 'delete'
     else:
-        wish_list.create(userID=user, wished_item=product, slug=wish_slug)
+        wish_list.create(userID=user, wished_item=product)
         context['method'] = 'create'
  
     return JsonResponse(context)
